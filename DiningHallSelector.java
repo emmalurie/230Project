551691cd7066f656abffe7bf9e7329a59495322c
Dorothysun <daxisun@hotmail.com>
2016-05-02 15:35:38 -0400

    comment

--- DiningHallSelector.java
+++ DiningHallSelector.java
@@ -63,6 +63,43 @@ public class DiningHallSelector {
             }
             }
   
+<<<<<<< Updated upstream
+=======
+  public static Hashtable<String,Dish> createHashtable(String inFileName){
+    
+    Hashtable<String,Dish> mealData = new Hashtable<String,Dish>(100); 
+    
+    
+    try {   // set up file for reading meals, one per line
+      Scanner reader = new Scanner(new File(inFileName));
+      String[] nameScore;
+      String line;
+      String name; 
+      int score;
+      while (reader.hasNext()){
+        line = reader.nextLine().trim();
+        
+        //System.out.println(line);
+        nameScore = line.split("\t");
+ 
+    
+        if(nameScore.length > 1){ //if contains a name and a score 
+        name = nameScore[0];
+        score = Integer.parseInt(nameScore[1]);
+        Dish value = new Dish (name, score);
+        mealData.put(name, value);
+        }
+
+      }
+      reader.close();    // close file
+      
+    } catch(FileNotFoundException ex){
+      System.out.println(inFileName + "is not found");
+    }
+    return mealData;
+  }
+  
+>>>>>>> Stashed changes
   
 
   
