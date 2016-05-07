6452d68401b1edfe5313a20029e8ba9b4edbe133
Emma Lurie <elurie@wellesley.edu>
2016-05-07 15:42:46 -0400

    updates

--- DiningHallSelectorTesting.java
+++ DiningHallSelectorTesting.java
@@ -129,7 +129,7 @@ public class DiningHallSelectorTesting{
     System.out.println("Saturday Dinner:\n" + DiningHallSelector.readWellesleyFresh("menus/tower.txt","dinner", 7) +"\n");
     */
 
-     /*
+     
      System.out.println("Bates Menu Scored");
      
      DiningHall bates = new DiningHall("Bates");
@@ -180,7 +180,7 @@ public class DiningHallSelectorTesting{
      bates = new DiningHall("Bates");
      bates.initializeDiningHall("menus/bates.txt", "data/Bates_data.tsv", "dinner", 7);
      System.out.println("Saturday Dinner:\n" + bates+"\n");
-     */
+     *//*
      System.out.println("Lulu Menu Scored");
      DiningHall lulu = new DiningHall("Lulu");
      lulu.initializeDiningHall("menus/bplc.txt", "data/Lulu_data.tsv", "lunch", 1);
@@ -217,6 +217,175 @@ public class DiningHallSelectorTesting{
      lulu = new DiningHall("Lulu");
      lulu.initializeDiningHall("menus/bplc.txt", "data/Lulu_data.tsv", "dinner", 5);
      System.out.println("Thursday Dinner:\n" + lulu+"\n");
+     
+      lulu = new DiningHall("Lulu");
+     lulu.initializeDiningHall("menus/bplc.txt", "data/Lulu_data.tsv", "lunch", 4);
+     System.out.println("Friday Lunch:\n" +lulu +"\n");
+     lulu = new DiningHall("Lulu");
+     lulu.initializeDiningHall("menus/bplc.txt", "data/Lulu_data.tsv", "dinner", 4);
+     System.out.println("Friday Dinner:\n" + lulu+"\n");
+     
+     lulu = new DiningHall("Lulu");
+     lulu.initializeDiningHall("menus/bplc.txt", "data/Lulu_data.tsv", "lunch", 7);
+     System.out.println("Saturday Lunch:\n" +lulu +"\n");
+     lulu = new DiningHall("Lulu");
+     lulu.initializeDiningHall("menus/bplc.txt", "data/Lulu_data.tsv", "dinner", 7);
+     System.out.println("Saturday Dinner:\n" + lulu+"\n");
+     *//*
+     System.out.println("Pom Menu Scored");
+     DiningHall pom = new DiningHall("Pomeroy");
+     pom.initializeDiningHall("menus/pomeroy.txt", "data/Pomeroy_Data.tsv", "lunch", 1);
+     System.out.println("Sunday Lunch:\n" +pom +"\n");
+     pom = new DiningHall("Pomeroy");
+     pom.initializeDiningHall("menus/pomeroy.txt", "data/Pomeroy_Data.tsv", "dinner", 1);
+     System.out.println("Sunday Dinner:\n" + pom +"\n");
+     
+
+     pom = new DiningHall("Pomeroy");
+     pom.initializeDiningHall("menus/pomeroy.txt", "data/Pomeroy_Data.tsv", "lunch", 2);
+     System.out.println("Monday Lunch:\n" +pom +"\n");
+     pom = new DiningHall("Pomeroy");
+     pom.initializeDiningHall("menus/pomeroy.txt", "data/Pomeroy_Data.tsv", "dinner", 2);
+     System.out.println("Monday Dinner:\n" + pom+"\n");
+     
+     pom = new DiningHall("Pomeroy");
+     pom.initializeDiningHall("menus/pomeroy.txt", "data/Pomeroy_Data.tsv", "lunch", 3);
+     System.out.println("Tuesday Lunch:\n" +pom +"\n");
+     pom = new DiningHall("Pomeroy");
+     pom.initializeDiningHall("menus/pomeroy.txt", "data/Pomeroy_Data.tsv", "dinner", 3);
+     System.out.println("Tuesday Dinner:\n" + pom+"\n");
+     
+     pom = new DiningHall("Pomeroy");
+     pom.initializeDiningHall("menus/pomeroy.txt", "data/Pomeroy_Data.tsv", "lunch", 4);
+     System.out.println("Wednesday Lunch:\n" +pom +"\n");
+     pom = new DiningHall("Pomeroy");
+     pom.initializeDiningHall("menus/pomeroy.txt", "data/Pomeroy_Data.tsv", "dinner", 4);
+     System.out.println("Wednesday Dinner:\n" + pom+"\n");
+     
+     pom = new DiningHall("Pomeroy");
+     pom.initializeDiningHall("menus/pomeroy.txt", "data/Pomeroy_Data.tsv", "lunch", 5);
+     System.out.println("Thursday Lunch:\n" +pom +"\n");
+     pom = new DiningHall("Pomeroy");
+     pom.initializeDiningHall("menus/pomeroy.txt", "data/Pomeroy_Data.tsv", "dinner", 5);
+     System.out.println("Thursday Dinner:\n" + pom+"\n");
+     
+     pom = new DiningHall("Pomeroy");
+     pom.initializeDiningHall("menus/pomeroy.txt", "data/Pomeroy_Data.tsv", "lunch", 4);
+     System.out.println("Friday Lunch:\n" +pom +"\n");
+     pom = new DiningHall("Pomeroy");
+     pom.initializeDiningHall("menus/pomeroy.txt", "data/Pomeroy_Data.tsv", "dinner", 4);
+     System.out.println("Friday Dinner:\n" + pom+"\n");
+     
+     pom = new DiningHall("Pomeroy");
+     pom.initializeDiningHall("menus/pomeroy.txt", "data/Pomeroy_Data.tsv", "lunch", 7);
+     System.out.println("Saturday Lunch:\n" +pom +"\n");
+     pom = new DiningHall("Lulu");
+     pom.initializeDiningHall("menus/pomeroy.txt", "data/Pomeroy_Data.tsv", "dinner", 7);
+     System.out.println("Saturday Dinner:\n" + pom+"\n");
+     
+     System.out.println("Stone Davis Menu Scored");
+     DiningHall stoned = new DiningHall("Stone Davis");
+     stoned.initializeDiningHall("menus/stonedavis.txt", "data/StoneDavis_Data.tsv", "lunch", 1);
+     System.out.println("Sunday Lunch:\n" +stoned +"\n");
+     stoned = new DiningHall("Stone Davis");
+     stoned.initializeDiningHall("menus/stonedavis.txt", "data/StoneDavis_Data.tsv", "dinner", 1);
+     System.out.println("Sunday Dinner:\n" + stoned +"\n");
+
+     stoned = new DiningHall("Stone Davis");
+     stoned.initializeDiningHall("menus/stonedavis.txt", "data/StoneDavis_Data.tsv", "lunch", 2);
+     System.out.println("Monday Lunch:\n" +stoned +"\n");
+     stoned = new DiningHall("Stone Davis");
+     stoned.initializeDiningHall("menus/stonedavis.txt", "data/StoneDavis_Data.tsv", "dinner", 2);
+     System.out.println("Monday Dinner:\n" + stoned+"\n");
+     
+     stoned = new DiningHall("Stone Davis");
+     stoned.initializeDiningHall("menus/stonedavis.txt", "data/StoneDavis_Data.tsv", "lunch", 3);
+     System.out.println("Tuesday Lunch:\n" +stoned +"\n");
+     stoned = new DiningHall("Stone Davis");
+     stoned.initializeDiningHall("menus/stonedavis.txt", "data/StoneDavis_Data.tsv", "dinner", 3);
+     System.out.println("Tuesday Dinner:\n" + stoned+"\n");
+     
+     stoned = new DiningHall("Stone Davis");
+     stoned.initializeDiningHall("menus/stonedavis.txt", "data/StoneDavis_Data.tsv", "lunch", 4);
+     System.out.println("Wednesday Lunch:\n" +stoned +"\n");
+     stoned = new DiningHall("Stone Davis");
+     stoned.initializeDiningHall("menus/stonedavis.txt", "data/StoneDavis_Data.tsv", "dinner", 4);
+     System.out.println("Wednesday Dinner:\n" + stoned+"\n");
+     
+     stoned = new DiningHall("Stone Davis");
+     stoned.initializeDiningHall("menus/stonedavis.txt", "data/StoneDavis_Data.tsv", "lunch", 5);
+     System.out.println("Thursday Lunch:\n" +stoned +"\n");
+     stoned = new DiningHall("Stone Davis");
+     stoned.initializeDiningHall("menus/stonedavis.txt", "data/StoneDavis_Data.tsv", "dinner", 5);
+     System.out.println("Thursday Dinner:\n" + stoned+"\n");
+     
+     stoned = new DiningHall("Stone Davis");
+     stoned.initializeDiningHall("menus/stonedavis.txt", "data/StoneDavis_Data.tsv", "lunch", 4);
+     System.out.println("Friday Lunch:\n" +stoned +"\n");
+     stoned = new DiningHall("Stone Davis");
+     stoned.initializeDiningHall("menus/stonedavis.txt", "data/StoneDavis_Data.tsv", "dinner", 4);
+     System.out.println("Friday Dinner:\n" + stoned+"\n");
+     
+     stoned = new DiningHall("Stone Davis");
+     stoned.initializeDiningHall("menus/stonedavis.txt", "data/StoneDavis_Data.tsv", "lunch", 7);
+     System.out.println("Saturday Lunch:\n" +stoned +"\n");
+     stoned = new DiningHall("Stone Davis");
+     stoned.initializeDiningHall("menus/stonedavis.txt", "data/StoneDavis_Data.tsv", "dinner", 7);
+     System.out.println("Saturday Dinner:\n" + stoned+"\n");
+     
+     
+     System.out.println("Tower Menu Scored");
+     DiningHall stoned = new DiningHall("Tower");
+     tower.initializeDiningHall("menus/tower.txt", "data/Tower_Data.tsv", "lunch", 1);
+     System.out.println("Sunday Lunch:\n" +tower +"\n");
+     tower = new DiningHall("Tower");
+     tower.initializeDiningHall("menus/tower.txt", "data/Tower_Data.tsv", "dinner", 1);
+     System.out.println("Sunday Dinner:\n" + tower +"\n");
+
+     tower = new DiningHall("Tower");
+     tower.initializeDiningHall("menus/tower.txt", "data/Tower_Data.tsv", "lunch", 2);
+     System.out.println("Monday Lunch:\n" +tower +"\n");
+     tower = new DiningHall("Tower");
+     tower.initializeDiningHall("menus/tower.txt", "data/Tower_Data.tsv", "dinner", 2);
+     System.out.println("Monday Dinner:\n" + tower+"\n");
+     
+     tower = new DiningHall("Tower");
+     tower.initializeDiningHall("menus/tower.txt", "data/Tower_Data.tsv", "lunch", 3);
+     System.out.println("Tuesday Lunch:\n" +tower +"\n");
+     tower = new DiningHall("Tower");
+     tower.initializeDiningHall("menus/tower.txt", "data/Tower_Data.tsv", "dinner", 3);
+     System.out.println("Tuesday Dinner:\n" + tower+"\n");
+     
+     tower = new DiningHall("Tower");
+     tower.initializeDiningHall("menus/tower.txt", "data/Tower_Data.tsv", "lunch", 4);
+     System.out.println("Wednesday Lunch:\n" +tower +"\n");
+     tower = new DiningHall("Tower");
+     tower.initializeDiningHall("menus/tower.txt", "data/Tower_Data.tsv", "dinner", 4);
+     System.out.println("Wednesday Dinner:\n" + tower+"\n");
+     
+     tower = new DiningHall("Tower");
+     tower.initializeDiningHall("menus/tower.txt", "data/Tower_Data.tsv", "lunch", 5);
+     System.out.println("Thursday Lunch:\n" +tower +"\n");
+     tower = new DiningHall("Tower");
+     tower.initializeDiningHall("menus/tower.txt", "data/Tower_Data.tsv", "dinner", 5);
+     System.out.println("Thursday Dinner:\n" + tower+"\n");
+     
+     tower = new DiningHall("Tower");
+     tower.initializeDiningHall("menus/tower.txt", "data/Tower_Data.tsv", "lunch", 4);
+     System.out.println("Friday Lunch:\n" +tower +"\n");
+     tower = new DiningHall("Tower");
+     tower.initializeDiningHall("menus/tower.txt", "data/Tower_Data.tsv", "dinner", 4);
+     System.out.println("Friday Dinner:\n" + tower+"\n");
+     
+     tower = new DiningHall("Tower");
+     tower.initializeDiningHall("menus/tower.txt", "data/Tower_Data.tsv", "lunch", 7);
+     System.out.println("Saturday Lunch:\n" +tower +"\n");
+     tower = new DiningHall("Stone Davis");
+     tower.initializeDiningHall("menus/tower.txt", "data/Tower_Data.tsv", "dinner", 7);
+     System.out.println("Saturday Dinner:\n" + tower+"\n");
+     
+     
+     
   }
   
   
